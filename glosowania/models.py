48669e8bebe553f7a9a4c0d1aafc20c785b41daa
Robert Fiałek <robert.fialek@gmail.com>
2019-05-05 22:08:16 +0200

    uzasadnienie

--- glosowania/models.py
+++ glosowania/models.py
@@ -7,50 +7,55 @@ base_dir = os.path.abspath('.')
 
 
 class Decyzja(models.Model):
-	autor = models.CharField(max_length=200)
-	tresc = models.TextField(max_length=500, null=True, verbose_name='Treść')
-	kara = models.TextField(max_length=500, null=True, verbose_name='Kara')  # Kara za nie przestrzeganie przepisu
-	ile_osob_podpisalo = models.SmallIntegerField(editable=False, default=0)
-	data_powstania = models.DateField(editable=False, null=True)
-	data_zebrania_podpisow = models.DateField(editable=False, null=True)
-	data_referendum_start = models.DateField(editable=False, null=True)
-	data_referendum_stop = models.DateField(editable=False, null=True)
-	data_zatwierdzenia = models.DateField(editable=False, null=True)
-	data_obowiazuje_od = models.DateField(editable=False, null=True)
-	za = models.SmallIntegerField(default=0, editable=False)
-	przeciw = models.SmallIntegerField(default=0, editable=False)
-	status = models.SmallIntegerField(default=1, editable=False)
+    autor = models.CharField(max_length=200)
+    tresc = models.TextField(max_length=500, null=True, verbose_name='Treść')
+    kara = models.TextField(max_length=500, null=True, verbose_name='Kara')
+    uzasadnienie = models.TextField(max_length=1500, null=True, verbose_name='Uzasadnienie')
+    ile_osob_podpisalo = models.SmallIntegerField(editable=False, default=0)
+    data_powstania = models.DateField(editable=False, null=True)
+    data_zebrania_podpisow = models.DateField(editable=False, null=True)
+    data_referendum_start = models.DateField(editable=False, null=True)
+    data_referendum_stop = models.DateField(editable=False, null=True)
+    data_zatwierdzenia = models.DateField(editable=False, null=True)
+    data_obowiazuje_od = models.DateField(editable=False, null=True)
+    za = models.SmallIntegerField(default=0, editable=False)
+    przeciw = models.SmallIntegerField(default=0, editable=False)
+    status = models.SmallIntegerField(default=1, editable=False)
 
-	# 0.Propozycja, 1.Brak poparcia, 2.W kolejce, 3.Referendum, 4.Odrzucone, 5.Zatwierdzone/Vacatio Legis, 6.Obowiązuje
+    # 0.Propozycja, 1.Brak poparcia, 2.W kolejce, 3.Referendum, 4.Odrzucone, 
+    # 5.Zatwierdzone/Vacatio Legis, 6.Obowiązuje
 
-	# TODO: data_wejscia_w_zycie = models.DateField(editable=False, default=dzis)
+    # TODO: data_wejscia_w_zycie =
+    #       models.DateField(editable=False, default=dzis)
 
-	def __str__(self):
-		return '%s: %s on %s' % (self.pk, self.tresc, self.status)
+    def __str__(self):
+        return '%s: %s on %s' % (self.pk, self.tresc, self.status)
 
-	class Meta:
-		verbose_name_plural = "Decyzje"
-	
-	objects = models.Manager() 
+    class Meta:
+        verbose_name_plural = "Decyzje"
+    
+    objects = models.Manager() 
 
 
 class ZebranePodpisy(models.Model):
-	'''Lista podpisów pod wnioskiem o referendum'''
-	projekt = models.ForeignKey(Decyzja, on_delete=models.CASCADE)
-	podpis_uzytkownika = models.ForeignKey(User, on_delete=models.CASCADE)  # odnotowujemy kto się podpisał
+    '''Lista podpisów pod wnioskiem o referendum'''
+    projekt = models.ForeignKey(Decyzja, on_delete=models.CASCADE)
 
-	class Meta:
-		unique_together = ('projekt', 'podpis_uzytkownika')
+    # Lets note who signed proposal:
+    podpis_uzytkownika = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('projekt', 'podpis_uzytkownika')
 
 
 class KtoJuzGlosowal(models.Model):
-	projekt = models.ForeignKey(Decyzja, on_delete=models.CASCADE)
-	ktory_uzytkownik_juz_zaglosowal = models.ForeignKey(User, on_delete=models.CASCADE)
+    projekt = models.ForeignKey(Decyzja, on_delete=models.CASCADE)
+    ktory_uzytkownik_juz_zaglosowal = models.ForeignKey(User, on_delete=models.CASCADE)
 
-	# odnotowujemy tylko fakt głosowania
+    # odnotowujemy tylko fakt głosowania
 
-	class Meta:
-		unique_together = ('projekt', 'ktory_uzytkownik_juz_zaglosowal')
+    class Meta:
+        unique_together = ('projekt', 'ktory_uzytkownik_juz_zaglosowal')
 
 
 #######################################################
@@ -87,8 +92,8 @@ Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date
 
 
 def pokaz_flow_f():
-	# webbrowser.open("https://i.imgur.com/FnVthWi.png")
-	print("\nStworzenie propozycji > Zbieranie podpisów > Kolejka i Dyskusja >")
-	print("> Referendum > Vacatio Legis > Przepis obowiązuje")
-	print('\nIle osób potrzeba do zaakceptowania/odrzucenia użytkownika:', AKCEPTUJACY)
-	input("\n\n[ENTER]")
+    # webbrowser.open("https://i.imgur.com/FnVthWi.png")
+    print("\nStworzenie propozycji > Zbieranie podpisów > Kolejka i Dyskusja >")
+    print("> Referendum > Vacatio Legis > Przepis obowiązuje")
+    print('\nIle osób potrzeba do zaakceptowania/odrzucenia użytkownika:', AKCEPTUJACY)
+    input("\n\n[ENTER]")
