344a10a7e15798191c7226fbc2c4701268655784
Robert Fiałek <robert.fialek@gmail.com>
2019-05-05 18:32:53 +0200

    tools

--- tools/recreate_database.sh
+++ tools/recreate_database.sh
@@ -3,24 +3,17 @@
 if [ "$1" != "" ]; then
     name=$1
     password=`pwgen 10 1`
-<<<<<<< HEAD
 
     # Drop database:
     sudo -u postgres psql -c "DROP DATABASE $name;"
     sudo -u postgres psql -c "DROP USER $name;"
 
-=======
-   
->>>>>>> 89f36f7d84066208d3b1cafd023f0c5a2e086e93
     # Create database and user:
     sudo -u postgres psql -c "CREATE USER $name WITH PASSWORD '$password';"
     sudo -u postgres psql -c "CREATE DATABASE $name;"
     sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $name TO $name;"
     
-<<<<<<< HEAD
     echo ""
-=======
->>>>>>> 89f36f7d84066208d3b1cafd023f0c5a2e086e93
     echo Database name: $name
     echo Database user: $name
     echo Database password: $password
