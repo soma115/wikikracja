4d42ee4156f490edd29990bf5c6710fef939a970
Robert Fiałek <robert.fialek@gmail.com>
2019-06-02 23:00:17 +0200

    reset pass bug1

--- zzz/urls.py
+++ zzz/urls.py
@@ -18,25 +18,8 @@ urlpatterns = [
   url(r'^glosowania/', include('glosowania.urls'), name='glosowania'),
   url(r'^obywatele/', include('obywatele.urls'), name='obywatele'),
   url(r'^elibrary/', include('elibrary.urls', namespace='elibrary')),
-  # reset password
-  # https://simpleisbetterthancomplex.com/tutorial/2016/09/19/how-to-create-password-reset-view.html
-  url(r'^password_reset/$',
-      auth_views.PasswordResetView.as_view(
-          template_name='home/password_reset_form.html'),
-      name='password_reset'),
-  url(r'^password_reset/done/$',
-      auth_views.PasswordResetDoneView.as_view(
-          template_name='home/password_reset_done.html'),
-      name='password_reset_done'),
-  url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-      auth_views.PasswordResetConfirmView.as_view(
-          template_name='home/password_reset_confirm.html'),
-      name='password_reset_confirm'),
-  url(r'^reset/done/$',
-      auth_views.PasswordResetCompleteView.as_view(
-          template_name='home/password_reset_complete.html'),
-      name='password_reset_complete'),
 
+  url(r'^chat/', include('chat.urls')),
 
   # url(r'^offer/new/$', onv.offer_new, name='offer_new'),
   # url(r'^offer/(?P<pk>[0-9]+)/$', onv.offer_detail, name='offer_detail'),
