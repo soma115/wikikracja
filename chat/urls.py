85c887cacce699a68842d025bda50ad0e5a3ac25
Robert Fiałek <robert.fialek@gmail.com>
2019-06-24 23:05:18 +0200

    chat-asgi

--- chat/urls.py
+++ chat/urls.py
@@ -6,9 +6,6 @@ app_name = 'chat'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
-
-    # url(r'^(?P<ogolny>)/$', views.room, name='ogolny'),
     url(r'^ogolny/$', views.room, name='ogolny'),
-    # url(r'^(?P<pk>[0-9]+)/$', v.obywatele_szczegoly,
 
 ]
