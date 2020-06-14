e01291e517658134689261c0c504f32a3c9768fa
Robert Fiałek <robert.fialek@gmail.com>
2020-06-14 18:10:00 +0200

    settings

--- zzz/wsgi.py
+++ zzz/wsgi.py
@@ -1,5 +1,5 @@
 import os
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zzz.settings")
-
-from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
