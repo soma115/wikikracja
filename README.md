8437ab2f8c19555677d126a1a75aee9b8b69deb0
Robert Fiałek <robert.fialek@gmail.com>
2020-09-26 11:50:18 +0200

    deploy

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ You will need email account like gmail in order to send emails to users.
 Smallest KVM VM is enough. 
 
 ## Installation (Centos 7, 8)
-- Run ./scripts/deploy_server.sh
+- download scripts/deploy_server.sh and run it as root
 - Set 'user' as default in /etc/nginx/nginx.conf (user user;)
 - adjust zzz/setting.py: Add SECRET_KEY etc.
 - run `cerbot --nginx`
--- scripts/deploy_server.sh
+++ scripts/deploy_server.sh
@@ -2,27 +2,26 @@
 
 # software
 dnf -y install epel-release 
-dnf -y install git-all python3 virtualenv nginx python3-certbot-nginx supervisord podman
+dnf -y install git-all python3 virtualenv nginx python3-certbot-nginx supervisor podman
 
 # user
 adduser user -s /sbin/nologin
 
-# configs
-cp ./scripts/etc/supervisord.d/wiki.ini /etc/supervisord.d/
-cp ./scripts/etc/nginx/conf.d/wiki.conf /etc/nginx/conf.d/
-
 # app folder
-mkdir /home/user/wiki
+mkdir /home/user/wiki  # TODO: as parameter or something
+cd /home/user/wiki
 
 # python
-git clone https://github.com/soma115/wikikracja.git /home/user/wiki/wiki/
-virtualenv -p python3 /home/user/wiki/venv
-source /home/user/wiki/venv/bin/activate
-pip3 install -r /home/user/wiki/wiki/requirements.txt
+git clone https://github.com/soma115/wikikracja.git
+virtualenv -p python3 venv
+source venv/bin/activate
+pip3 install -r wiki/requirements.txt
 
 # user rights etc.
-/home/user/wiki/wiki/scripts/update.sh
+./wiki/scripts/update.sh
 
 # services
+cp wiki/scripts/etc/supervisord.d/wiki.ini /etc/supervisord.d/
+cp wiki/scripts/etc/nginx/conf.d/wiki.conf /etc/nginx/conf.d/
 systemctl enable nginx supervisord
 ./scripts/restart_services.sh
