66cfa7f2a32886a5113ed8d3014bc02df5e6a097
soma115 <robert.fialek@gmail.com>
2021-07-29 18:20:39 +0200

    acceptance_simulator

--- scripts/acceptance_simulator.py
+++ scripts/acceptance_simulator.py
@@ -1,10 +1,11 @@
 from math import log
-ACCEPTANCE_MULTIPLIER = 0.3
+ACCEPTANCE_MULTIPLIER = 0.5
 
 print('Population | Required acceptance')
 for i in range(1, 10):
     print(f'{str(i).rjust(10)} | {str(round(log(i)*ACCEPTANCE_MULTIPLIER)+1).rjust(1)}')
     # +1 because acceptance has to be > than threshold. +1 is just for simulation.
+    # print(f'{str(i).rjust(10)} | {str(round(log(i)*ACCEPTANCE_MULTIPLIER)).rjust(1)}')
 
     # print(f' {str(i).rjust(9)} | {str(round(i*ACCEPTANCE_MULTIPLIER)+1).rjust(1)}')
 
