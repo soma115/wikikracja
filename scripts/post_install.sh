82afb8fddabe7a7486194ad0971c87e7fa5f7007
soma115 <robert.fialek@gmail.com>
2022-03-28 14:12:30 +0200

    post_install

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
 DOMAIN=`grep ALLOWED_HOSTS zzz/settings_custom.py | cut -d "'" -f 2`
-APP=`echo ${DOMAIN} | cut -d '.' -f 1`
-HOME=/home/user/$APP/
+HOME=/home/user/${DOMAIN}/
 
 # DEPLOY DB
-cd $HOME/$APP; $HOME/venv/bin/python $HOME/$APP/manage.py makemigrations article;
-cd $HOME/$APP; $HOME/venv/bin/python $HOME/$APP/manage.py makemigrations chat;
-cd $HOME/$APP; $HOME/venv/bin/python $HOME/$APP/manage.py makemigrations customize;
-cd $HOME/$APP; $HOME/venv/bin/python $HOME/$APP/manage.py makemigrations elibrary;
-cd $HOME/$APP; $HOME/venv/bin/python $HOME/$APP/manage.py makemigrations glosowania;
-cd $HOME/$APP; $HOME/venv/bin/python $HOME/$APP/manage.py makemigrations obywatele;
-cd $HOME/$APP; $HOME/venv/bin/python $HOME/$APP/manage.py makemigrations;
-cd $HOME/$APP; $HOME/venv/bin/python $HOME/$APP/manage.py migrate;
-cd $HOME/$APP; $HOME/venv/bin/python $HOME/$APP/manage.py makemessages -l 'en' --ignore=.git/* --ignore=static/* --ignore=.mypy_cache/*;
-cd $HOME/$APP; $HOME/venv/bin/python $HOME/$APP/manage.py makemessages -l 'pl' --ignore=.git/* --ignore=static/* --ignore=.mypy_cache/*;
-cd $HOME/$APP; $HOME/venv/bin/python $HOME/$APP/manage.py compilemessages --ignore=.git/* --ignore=static/* --ignore=.mypy_cache/*;
+cd ${HOME}/${DOMAIN}; ${HOME}/venv/bin/python ${HOME}/${DOMAIN}/manage.py makemigrations article;
+cd ${HOME}/${DOMAIN}; ${HOME}/venv/bin/python ${HOME}/${DOMAIN}/manage.py makemigrations chat;
+cd ${HOME}/${DOMAIN}; ${HOME}/venv/bin/python ${HOME}/${DOMAIN}/manage.py makemigrations customize;
+cd ${HOME}/${DOMAIN}; ${HOME}/venv/bin/python ${HOME}/${DOMAIN}/manage.py makemigrations elibrary;
+cd ${HOME}/${DOMAIN}; ${HOME}/venv/bin/python ${HOME}/${DOMAIN}/manage.py makemigrations glosowania;
+cd ${HOME}/${DOMAIN}; ${HOME}/venv/bin/python ${HOME}/${DOMAIN}/manage.py makemigrations obywatele;
+cd ${HOME}/${DOMAIN}; ${HOME}/venv/bin/python ${HOME}/${DOMAIN}/manage.py makemigrations;
+cd ${HOME}/${DOMAIN}; ${HOME}/venv/bin/python ${HOME}/${DOMAIN}/manage.py migrate;
+cd ${HOME}/${DOMAIN}; ${HOME}/venv/bin/python ${HOME}/${DOMAIN}/manage.py makemessages -l 'en' --ignore=.git/* --ignore=static/* --ignore=.mypy_cache/*;
+cd ${HOME}/${DOMAIN}; ${HOME}/venv/bin/python ${HOME}/${DOMAIN}/manage.py makemessages -l 'pl' --ignore=.git/* --ignore=static/* --ignore=.mypy_cache/*;
+cd ${HOME}/${DOMAIN}; ${HOME}/venv/bin/python ${HOME}/${DOMAIN}/manage.py compilemessages --ignore=.git/* --ignore=static/* --ignore=.mypy_cache/*;
 
 # FIXTURES
 # Carefull - it will duplicate existing records if run second time - especialy if you'll use: "pk": null,
 # TODO: grep settings.py to find out which language should be deployed
 # Dump: ./manage.py dumpdata customize.customize > customize.json  # customize.customize is a table
 # ./manage.py loaddata article/fixtures/articles.json
-cd $HOME/$APP; $HOME/venv/bin/python $HOME/$APP/manage.py loaddata $HOME/$APP/customize/fixtures/customize.json
+cd ${HOME}/${DOMAIN}; ${HOME}/venv/bin/python ${HOME}/${DOMAIN}/manage.py loaddata ${HOME}/${DOMAIN}/customize/fixtures/customize.json
 
 # SITE NAME
 # Change example.com to actuall domain name
@@ -33,10 +32,10 @@ certbot --nginx --quiet --agree-tos --domains ${DOMAIN}
 
 # VOTINGS
 # Fixtures z początkowymi głosowaniami do wiki
-cd $HOME/$APP; $HOME/venv/bin/python $HOME/$APP/manage.py loaddata $HOME/$APP/glosowania/fixtures/votings.json
+cd ${HOME}/${DOMAIN}; ${HOME}/venv/bin/python ${HOME}/${DOMAIN}/manage.py loaddata ${HOME}/${DOMAIN}/glosowania/fixtures/votings.json
 
 # COLLECT STATIC
-cd $HOME/$APP; $HOME/venv/bin/python $HOME/$APP/manage.py collectstatic --no-input -v 0 --no-post-process -i *bootstrap.css
+cd ${HOME}/${DOMAIN}; ${HOME}/venv/bin/python ${HOME}/${DOMAIN}/manage.py collectstatic --no-input -v 0 --no-post-process -i *bootstrap.css
 
 # RESTART INSTANCE
-supervisorctl restart ${APP}:asgi0
+supervisorctl restart ${DOMAIN}:asgi0
